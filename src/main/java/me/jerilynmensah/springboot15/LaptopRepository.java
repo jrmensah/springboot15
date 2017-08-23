4b67c5b1da3d955a87c15b0d8355e1dd98202bee
jrmensah <jmensah4@student.umuc.edu>
2017-08-23 12:18:57 -0400

    First commit-Database Relationships-One to One

---
+++ src/main/java/me/jerilynmensah/springboot15/LaptopRepository.java
@@ -0,0 +1,6 @@
+package me.jerilynmensah.springboot15;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LaptopRepository extends CrudRepository<Laptop, Long> {
+}
---
+++ src/main/java/me/jerilynmensah/springboot15/Springboot15Application.java
@@ -0,0 +1,12 @@
+package me.jerilynmensah.springboot15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot15Application.class, args);
+	}
+}
